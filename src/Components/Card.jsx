05923e93bd6662a787bdc881a1df16fabcf2fedb
Waqas7281 <wa1427621@gmail.com>
2025-05-17 22:22:31 +0500

    next update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -23,8 +23,8 @@ const Card = () => {
         data.map((item,index)=>{
           return(
             <div className='border-2 border-gray-900 bg-gray-900 sm:grid-cols-3 h-[12rem] w-[18rem] rounded-xl' key={index}>
-              <p className='text-[75%] flex justify-center pt-[10%] text-orange-500'>{item.titel}</p>
-              <p className='text-[70%]  flex justify-center pt-[10%]' >{item.frameWork}</p>
+              <p className='text-[85%] flex justify-center pt-[10%] font-bold text-orange-500'>{item.titel}</p>
+              <p className='text-[85%]  flex justify-center font-bold pt-[10%]' >{item.frameWork}</p>
             </div>
           )
         })
